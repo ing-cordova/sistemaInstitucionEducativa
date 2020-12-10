9ec54520b2d4152fe579275d1a47be043cbe4388
Dayana Ivette <63428525+dayanaivette@users.noreply.github.com>
2020-12-10 09:26:04 -0600

    ColorCeldas

---
+++ sistemaAdmonEscolar/src/main/java/com/unab/edu/Vistas/Alumno/ColorCeldas.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.Vistas.Alumno;
+
+/**
+ *
+ * @author dayan
+ */
+public class ColorCeldas {
+    
+}
